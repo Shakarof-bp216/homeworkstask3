38288fcfec2e6755abd584bd25df1259faaa96a2
Shakarof_bp216 <yalcinys-bp216@code.edu.az>
2024-10-21 08:56:07 +0400

    added

---
+++ index.js
@@ -0,0 +1,72 @@
+// 1 den 100 e qeder edeleri ekrana cixarin hem for ile hem while ile 
+
+for (i = 1; i <= 100; i++) {
+    console.log(i);
+
+}
+
+
+i = 0
+while (i < 100) {
+    i++
+
+    console.log(i);
+}
+
+
+// bir array yazin ve o array daki en kicik ededi tapan program yazin
+
+const arr1 = [21, 4, 5, 46, , 768, 723, 55, 39]
+
+for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] < arr1[i + 1]) {
+        console.log(arr1[i])
+    }
+
+    else {
+        break
+    }
+}
+
+
+
+
+// bir array yazin ve o arraydaki en boyuk ededi tapan program yazin
+
+
+
+
+
+// bir eded secin ve o ededin sade mi murekkeb mi oldugunu tapan program yazin ( sade eded sadece ozune ve 1 e bolunen ededir 11,13 ornek)
+let a = 7
+
+for (i = 2; i < a; i++) {
+
+    if (a % i == 0) {
+        console.log(a, "ededi mürekkebdir ");
+        break
+    }
+    else{
+        console.log(a, "ededi sadedir ");
+
+    }
+}
+
+
+// bir array  yazin qarmasiq olsun o arrayi artan sira ile duzen program yazin
+
+
+
+
+
+// 1  den n e qeder ededlerin icindeki sade  ededleri tapan program yazin 
+// (ornek 1 den 20 e kimi sade ededler  2,3,5,7,11,13,17,19)
+
+let n=40
+for (i = 0 ; i < n ; i++){
+
+
+}
+
+
+
